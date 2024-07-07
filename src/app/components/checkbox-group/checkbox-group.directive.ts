2156b5b679f100e4ea356364b48d69d3f835992f
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-07-07 13:21:20 +0200

    POC ready

--- src/app/components/checkbox-group/checkbox-group.directive.ts
+++ src/app/components/checkbox-group/checkbox-group.directive.ts
@@ -1,10 +1,11 @@
-import { Directive, contentChildren, forwardRef, input } from '@angular/core';
+import { ContentChildren, Directive, QueryList, contentChildren, forwardRef, input } from '@angular/core';
 import { BsCheckboxComponent } from '../checkbox/checkbox.component';
 
 @Directive({
     selector: '[bsCheckboxGroup]',
 })
 export class BsCheckboxGroupDirective {
-    // name = input.required<string>();
-    // children = contentChildren(forwardRef(() => BsCheckboxComponent));
+    name = input.required<string>();
+    children = contentChildren(forwardRef(() => BsCheckboxComponent));
+    // @ContentChildren(forwardRef(() => BsCheckboxComponent)) children!: QueryList<BsCheckboxComponent>;
 }
--- src/app/components/checkbox/checkbox.component.ts
+++ src/app/components/checkbox/checkbox.component.ts
@@ -3,11 +3,12 @@ import { Component, computed, input } from "@angular/core";
 @Component({
     selector: 'bs-checkbox',
     template: `
-        <label [class.form-switch]="isSwitch()">
+        <!-- <label [class.form-switch]="isSwitch()"> -->
+        <label>
             <input type="checkbox">
         </label>`
 })
 export class BsCheckboxComponent {
-    displayStyle = input<'checkbox' | 'switch' | 'toggle_button'>('checkbox');
-    isSwitch = computed(() => this.displayStyle() === 'switch');
+    // displayStyle = input<'checkbox' | 'switch' | 'toggle_button'>('checkbox');
+    // isSwitch = computed(() => this.displayStyle() === 'switch');
 }
